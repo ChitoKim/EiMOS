9f840779e231b87c70b1cb482560f82eaa6158a0
Chito Kim <nekroztrap@gmail.com>
2025-07-01 13:57:09 +0900

    Remove the superclass EiMOS

--- src/EiMOS.h
+++ src/EiMOS.h
@@ -5,46 +5,17 @@
 // Formerly known as mahjongAsst
 // Renamed 6 June, 2024
 
-class EiMOS
-{
-  private:
-  EiMOS_MEAS *meas;
-  EiMOS_DP *dp;
-  public:
-  EiMOS(EiMOS_MEAS *meas, EiMOS_DP *dp) : meas(meas), dp(dp) { }
-  EiMOS(EiMOS_MEAS meas, EiMOS_DP dp) : meas(&meas), dp(&dp) { }
-  EiMOS_MEAS *getMeas()
-  {
-    return this->meas;
-  }
-  EiMOS_DP *getDP()
-  {
-    return this->dp;
-  }
-  void measure()
-  {
-    meas->measure();
-  }
-  void show()
-  {
-    dp->show();
-  }
-  void loop()
-  {
-    measure();
-    show();
-  }
-};
-
 //Two interface classes; EiMOS_MEAS for measurements and EIMOS_DP for displays
 class EiMOS_MEAS
 {
   public:
+  EiMOS_MEAS() {}
   virtual void measure();
 };
 class EiMOS_DP
 {
   public:
+  EiMOS_DP() {}
   virtual void show();
 };
 #endif
