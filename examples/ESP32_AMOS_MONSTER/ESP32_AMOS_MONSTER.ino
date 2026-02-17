fee0e61d97b5ca7470570679b4ae47d9ec93699b
Chito Kim <76689397+ChitoKim@users.noreply.github.com>
2026-02-17 21:17:19 +0900

    Reform with interface classes (#19)
    * feat(score): 변경 R_REF, weight 2차원배열 (#14)
    
    * feat(score): 점수 측정 디바운싱 (#15)
    
    * feat(rule): 추가 3인마작, 토비나시 (#16)
    
    * Relicense to MIT
    
    * Reform EiMOS with interface classes
    
    * Remove the superclass EiMOS
    
    * Some modifications; made u8x8 interface compilable
    
    * Code formatting
    
    * Add another show method for debugging
    
    * fix .clang-format
    
    * properly initialise Results struct
    
    * Reform EiMOS with interface classes
    
    * Remove the superclass EiMOS
    
    * Some modifications; made u8x8 interface compilable
    
    * Code formatting
    
    * Add another show method for debugging
    
    * fix .clang-format
    
    * REF_CORRECTION_DIMENTION default to 1
    
    * Redo the struct definitions according to main branch
    
    * add measure method for EiMOS_RLC
    
    * Add getResults function
    
    * Modify examples
    
    ---------
    
    Co-authored-by: 마히로 <83218385+mahirocon@users.noreply.github.com>

--- examples/ESP32_AMOS_MONSTER/ESP32_AMOS_MONSTER.ino
+++ examples/ESP32_AMOS_MONSTER/ESP32_AMOS_MONSTER.ino
@@ -1,4 +1,4 @@
-#include <EiMOS.h>
+#include <EiMOS_RLC.h>
 int button_mode[] = {
   32, 33, 25, 26
   // 36, 39, 34, 35 are input only pins without internal pullup.
@@ -19,7 +19,7 @@ ADS1115 ADC2(0x48, &Wire1);
 ADS1115 ADC3(0x49, &Wire1);
 
 ADS1X15 *adc[] = {&ADC0, &ADC1, &ADC2, &ADC3};
-EiMOS EM(adc, RES_AMOS_MONSTER, R_REF);
+EiMOS_RLC RLC(adc, RES_AMOS_MONSTER, R_REF);
 
 float weight[] = {0.3f, 0.3f, 0.3f, 0.3f};
 enum I2CPIN
@@ -33,32 +33,32 @@ enum I2CPIN
 void
 setup()
 {
-  EM.setNSlot(4); // set number of slots; 5k 1k 100 (NSLOT = 3), or 5k 1k 100 100 (NSLOT = 4)
+  RLC.setNSlot(4); // set number of slots; 5k 1k 100 (NSLOT = 3), or 5k 1k 100 100 (NSLOT = 4)
 
-  EM.setMesType(RES);     // choose measure type; resistance(RES) or CAP(capacitance)
-  EM.setPullType(PULLUP); // choose whether to pull up or down the reference resistors
+  RLC.setMesType(RES);     // choose measure type; resistance(RES) or CAP(capacitance)
+  RLC.setPullType(PULLUP); // choose whether to pull up or down the reference resistors
                           // one of these: PULLUP, PULLDOWN, INPUT_PULLUP
 
-  // EM.setOffset(200); // uncomment to enable busting sticks
-  EM.setWeight(weight);
-  // EM.setModeButton(button_mode); // uncomment to enable mode buttons
-  // EM.setHonbaButton(button_honba); // uncomment to enable a honba button
+  // RLC.setOffset(200); // uncomment to enable busting sticks
+  RLC.setWeight(weight);
+  // RLC.setModeButton(button_mode); // uncomment to enable mode buttons
+  // RLC.setHonbaButton(button_honba); // uncomment to enable a honba button
 
   Wire.begin(_SDA0, _SCL0);
   Wire1.begin(_SDA1, _SCL1); // tweak pins_arduino.h for second I2C
 
-  EM.initExtADC();
-  EM.setExtADC(/*setGain*/ 1, /*ADC Resolution*/ 16, /*VCC*/ 3.3f);
-  EM.begin();
+  RLC.initExtADC();
+  RLC.setExtADC(/*setGain*/ 1, /*ADC Resolution*/ 16, /*VCC*/ 3.3f);
+  RLC.begin();
 }
 void
 loop()
 {
   int score[4] = {0};
   int error[4] = {0};
-  EM.loop(500);
-  EM.getScore(score);
-  EM.getError(error);
+  RLC.loop(500);
+  RLC.getScore(score);
+  RLC.getError(error);
   // Now you have scores and errors.
   // Display them anyway you like
 }
--- src/EiMOS.h
+++ src/EiMOS.h
@@ -1,133 +1,40 @@
-/*
-Special Thanks to Mahjong
+#ifndef EIMOS_H
+#define EIMOS_H
 
-EiMOS Library - EiMOS.h
-Formerly known as mahjongAsst
-Renamed 6 June, 2024
+// EiMOS Library - EiMOS.h
+// Formerly known as mahjongAsst
+// Renamed 6 June, 2024
 
-A Library for legacy scoring system of Japanese mahjong tables.
-Legacy mahjong scorers implement special score sticks containing electrical elements such as R, L, or C.
-This library measures parallel resistances/capacitances of stack-piled score sticks,
-and convert the values into actual scores of 4 mahjong players.
-
-Copyright (c) 2023, Chito Kim
-All rights reserved.
-
-Codes for capacitance measurement is based on an example code of Jonathan Nethercott.
-https://wordpress.codewrite.co.uk/pic/2014/01/25/capacitance-meter-mk-ii/
-*/
-
-#ifndef _MAHJONGASST_H
-#define _MAHJONGASST_H
-
-#if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_MBED_RP2040) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_STM32)
-#define ADC_RESOLUTION_MUTABLE
-#endif
-
-#include "ADS1X15.h"
-#include "MUX.h"
-#include "components.h"
-
-#define DEFAULT_ADC_MAX 1024
-#define DEFAULT_NSLOT 4
-#define DEFAULT_NUMPIN 16
-#define DEFAULT_HONBA 0
-#define MAXSTICK 50
-#define MAXSTICK_100P 50
-
-#define RES 0
-#define CAP 1
-#define PIN_NONE -1
-
-#ifndef PULLUP
-#define PULLUP 5
-#endif
-
-#ifndef PULLDOWN
-#define PULLDOWN -1
-#endif
-// INPUT_PULLUP
-
-#define MAXTIME sizeof(unsigned long)
+// Two interface classes; EiMOS_MEAS for measurements and EIMOS_DP for displays
 
 #define NORMAL 1
 #define DIFF 0
 #define PM 2
 #define SUM 1000
-
-class EiMOS
+typedef struct Results
 {
- private:
-  MUX *mux_p;
-  ENV *env_p;
-  PIN *pin_p;
-  VAL *val_p;
-
+  int score[4];
+  int error[4]; // checks if the sticks are in the right places; if error 1, if not 0
+  int mode[4];  // display mode of the scores : NORMAL, DIFF(score difference), PM(+/-)
+  int honba;
+  unsigned int totalScore;
+  int emptySeat;
+} Results;
+class EiMOS_MEAS
+{
  public:
-  EiMOS(MUX *mux, ENV *env, PIN *pin, VAL *val);
-  EiMOS(int charge[], int analog[], float v_unit[], float ref[]);
-  EiMOS(int charge[], int analog, float v_unit[], float ref[]);
-  EiMOS(int analog, float v_unit[], float ref[]);
-  EiMOS(int analog[], float v_unit[], float ref[]);
-  EiMOS(int charge[], ADS1X15 *ext_adc[], float v_unit[], float ref[]);
-  EiMOS(ADS1X15 *ext_adc[], float v_unit[], float ref[]);
-  MUX *getMUX();
-  ENV *getENV();
-  PIN *getPIN();
-  VAL *getVAL();
-  void setMUX4051(int a[], int b[]);
-  void setMUX4067(int a[]);
-  void initMUX();
-  void initExtADC();
-  void slotSelect(int slot_num);
-  void setNSlot(int a);
-  void setPullType(int a);
-  void setMesType(int a);
-  void setModeButton(int a[]);
-  void setHonbaButton(int a);
-  void setADCResolution(int a);
-  void setExtADC(int gain, int bit, float vcc, int mode = 0);
-  void setWeight(float a[]);
-  void setOffset(int a);
-
-  void getScore(int scr[]);
-  int *getScore();
-  void getError(int err[]);
-  int *getError();
-  void getMode(int mode[]);
-  int *getMode();
-  int getHonba();
-
-  int boolRead(int pin);
-  uint16_t adcRead(int pin);
-  uint16_t extADCRead(int slot_num);
-  void pullAnalog(int apin);
-
-  void mesLoop(float RLC[]);
-  void numLoop(float RLC[], int num[]);
-  void scoreLoop(int num[]);
-  void modeLoop();
-  void loop();
-  void loop(int period_ms);
-  void loop(float RLC[], int num[]);
-
-  void begin();
-  void prepMes(int slot_num);
-  float mesRLC(int slot_num);
-  int RLCToNum(float RLC, int slot_num);
-
-  // resistance specific////
-  ////
-  float adcToRes(uint16_t adc, float r);
-  // capacitance specific///
-  ////
-  void setParRes(float f[]);
-  int hasParRes(float f);
-  float adcToCap(unsigned long t, uint16_t adc, float r);
-  float correctCap(float f, float r, float ref);
-  void discharge(int cpin, int apin);
-  void charge(int cpin);
-  //
+  EiMOS_MEAS()
+  {
+  }
+  virtual void measure();
 };
-void _HONBA();
+class EiMOS_DP
+{
+ public:
+  EiMOS_DP()
+  {
+  }
+  virtual void show(Results *results);
+  virtual void show(const char *str);
+};
 #endif
